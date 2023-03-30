2c27b5aef5350776dfde4eb8162d68427e62ce73
nilcy <takehiro.shimokawa@gmail.com>
2023-03-31 01:15:38 +0900

    fetch from mongodb. TODO template literal

---
+++ packages/web/kamuy-web/src/routes/user/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<h1>id={JSON.stringify(data.user['_id'])}</h1>
+<h1>email={JSON.stringify(data.user['email'])}</h1>
+<h1>name={JSON.stringify(data.user['name'])}</h1>

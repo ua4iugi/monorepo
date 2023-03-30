4d34b7dfb89599789021f53d996c5813e1faa8ae
nilcy <takehiro.shimokawa@gmail.com>
2023-03-31 01:14:38 +0900

    add layout

---
+++ packages/web/kamuy-web/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<header>
+	<nav>
+		<a href="/"><img alt="Kamuy" src="https://via.placeholder.com/200x70?text=Kamuy" height="70" /></a>
+		<ul>
+			<li><a href="/">Home</a></li>
+		</ul>
+	</nav>
+</header>
+<main><slot /></main>
+<footer>Contact info</footer>

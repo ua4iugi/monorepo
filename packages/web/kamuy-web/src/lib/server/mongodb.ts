6123e3e37446a2435780ac5ab24fcdd02c034bbf
nilcy <takehiro.shimokawa@gmail.com>
2023-04-01 15:55:58 +0900

    s/Db/db/

--- packages/web/kamuy-web/src/lib/server/mongodb.ts
+++ packages/web/kamuy-web/src/lib/server/mongodb.ts
@@ -1,14 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { get } from 'svelte/store';
 import { accessToken } from '$lib/stores';
 
-export async function mongoDb(body: string) {
-	const token = get(accessToken);
+export async function mongodb(fetch: any, body: string): Promise<Request> {
 	return fetch(
 		'https://ap-southeast-1.aws.realm.mongodb.com/api/client/v2.0/app/data-gipvp/graphql',
 		{
 			method: 'POST',
 			headers: {
-				Authorization: `Bearer ${token}`,
+				Authorization: `Bearer ${get(accessToken)}`,
 				'Content-Type': 'application/json'
 			},
 			body: body
